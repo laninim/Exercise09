55c80bd8f8659b3cc6320c7b790120400d335291
Mauro Lanini <mallettomachico@gmail.com>
2022-11-06 21:37:25 +0100

    Final commit

---
+++ src/Animal.java
@@ -0,0 +1,22 @@
+public class Animal {
+
+    private String animalName;
+
+    public Animal(String animalName){
+        this.animalName = animalName;
+    }
+
+    public void animalSound(){
+        System.out.println("Roarr!");
+    }
+
+    public void animalSound(String intensity){
+        if(intensity.equalsIgnoreCase("high")){
+            System.out.println("Roarrrrrrrr!");
+        }else if(intensity.equalsIgnoreCase("low")){
+            System.out.println("Roar");
+        }else{
+            return;
+        }
+    }
+}
